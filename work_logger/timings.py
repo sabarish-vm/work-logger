540af4676958e2e27d7738d4b2c1b6cf8e5be0a8
sabarish-vm <sabarish.science@gmail.com>
2023-08-21 11:26:22 +0200

    add comments using -c flag

--- work_logger/timings.py
+++ work_logger/timings.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pandas as pd
+from argparse import ArgumentParser, RawTextHelpFormatter
 import os
 tIMINGSDIR=os.path.join(os.path.expanduser('~'),'.timings')
 todayFile=os.path.join(tIMINGSDIR,'today.csv')
@@ -45,12 +46,23 @@ def toHumanTime(timeInSecs):
     return f"{timeInSecs//60//60:2.0f}:{timeInSecs//60 - (timeInSecs//60//60*60):2.0f}"
 
 def imin() :
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-c',dest='comment',type=str,
+                        help="Pass comments",
+                        )
+    args = parser.parse_args()
+    comment = args.comment
+    print(comment)
     df=todayLoader(todayFile)
     length = df.__len__()
     ind=length-1
-    if length ==0 : df.loc[0,'In'] = dt.datetime.now().isoformat()
+    if length ==0 : 
+        df.loc[0,'In'] = dt.datetime.now().isoformat()
+        df.loc[0,'Comments'] = comment
     elif pd.isna(df.loc[ind]['Out']) : raise Exception("Out time not logged !")
-    else : df.loc[ind+1,'In'] = dt.datetime.now().isoformat()
+    else : 
+        df.loc[ind+1,'In'] = dt.datetime.now().isoformat()
+        df.loc[0,'Comments'] = comment
     df.to_csv(todayFile,index=False)
     return df
 
